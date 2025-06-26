d602ec42ed1d1b7410aa1402c039b51b5eb97266
Annette Hawks <hawksannette@gmail.com>
2025-06-25 20:42:58 -0600

    The Rules

--- Program.cs
+++ Program.cs
@@ -3,3 +3,31 @@
 //lab-4-madlibs-1-SpringHawks
 
 //Mad Libs: Vacation 
+
+//Annette Hawks
+//Due 6/25/2025
+//lab-4-madlibs-1-SpringHawks
+// 
+//Mad Libs: Vacation 
+
+//1. comment Add a comment to the top of your source code with your name, the date, and the name of the lab.
+//(Remember to run, add, and commit your code.)
+//2. greeting Print a greeting and description of the program to the user. (Remember to run, add, and commit.)
+//3. taking the string apart Here is a code snippet to load the story into a single string:
+//string originalStory = "A vacation is when you take a trip to some (adjective) place with your
+//(adjective) family. Usually, you go to some place that is near a/an (noun) or up on a/an (noun).
+//A good vacation place is one where you can ride (plural noun) or play (game) or go hunting for (plural noun).
+//I like to spend my time (verb ending in “ing”) or (verb ending in “ing”).
+//When parents go on a vacation, they spend their time eating three (plural noun) a day, and fathers play golf,
+//and mothers sit around (verb ending in “ing”) Last summer, my little brother fell in a/an (noun) and got poison
+//(plant) all over his (part of the body) My family is going to go to (place) and I will practice (verb ending in “ing”)
+//Parents need vacations more than kids because parents are always very (adjective) and because they have to work
+//(number) hours every day all year making enough (plural noun) to pay for the vacation.";
+//You may use this madlibs example or make up your own
+//string[] storyWords = originalStory.Split(' '); // This should hopefully split original story into words
+//4. analyze each word- look at each word and consider the first character.
+//If it not a parenthesis add it to the 'newStory' along with a space
+//If it is a parenthesis, ask the user to provide you with an example of the word type in the
+//parenthesis and add that word to the 'newStory' along with a space
+//5. display the new story- Print it out when you're done story can you make it print "frog."
+//with the period when it prints out the story at the end? (+5 extra credit)
